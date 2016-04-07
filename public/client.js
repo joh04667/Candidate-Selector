4229677167834822e3fff85a9e341552cf9f6a9b
Kyle Johnson <joh04667@gmail.com>
2016-04-07 16:27:46 -0500

    first commit

---
+++ public/client.js
@@ -0,0 +1,8 @@
+var app = angular.module("myApp", []);
+
+app.controller("N64Controller", function() {
+
+
+
+
+}); // n64
---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+  console.log(('req recieved'));
+  res.sendFile(path.join(__dirname, './index.html'));
+});
+
+
+
+
+
+
+//server listening
+var server = app.listen(process.env.PORT || 3000, function() {
+  var port = server.address().port;
+  console.log('listening on port', port);
+});
